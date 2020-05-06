df591c76fb575ff8b69635e301d4a22db07e591a
BackAged <brotherapes@gmail.com>
2020-05-06 16:00:33 +0600

    Implement json web token manager

---
+++ user-auth/src/infrastucture/json-web-token/json-web-token.ts
@@ -0,0 +1,20 @@
+import { GenerateJSONWebToken } from "../../usecase/registerUser/port";
+import jsonwebtoken from "jsonwebtoken";
+
+// hard coded for now
+const jwtKey = "blahblah";
+const jwtExpirySeconds = 3000;
+
+class JsonWebTokenManager implements GenerateJSONWebToken {
+    generateJSONWebTOken(payload: any): string {
+        return jsonwebtoken.sign(payload, jwtKey, {
+            algorithm: "HS256",
+		    expiresIn: jwtExpirySeconds,
+        })
+    }
+}
+
+
+export const newJsonWebTokenManager = () => {
+    return new JsonWebTokenManager();
+}
