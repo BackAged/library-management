f6092b6c13da59d490d6534226bc57784abd73fb
BackAged <brotherapes@gmail.com>
2020-05-06 14:57:13 +0600

    Implement RegisterUser UseCase

---
+++ user-auth/src/usecase/registerUser/registerUserUseCase.ts
@@ -0,0 +1,61 @@
+import { CreateUser, GenerateJSONWebToken } from "./port";
+import { User } from "../../entity/user";
+
+// kinda like DTO
+export interface RegisterUserUseCaseRequest {
+    name: string,
+    age: number,
+    gender: string,
+    email: string,
+    phone?: string,
+}
+
+// kinda like DTO
+export interface RegisterUserUseCaseResponse {
+    user: {
+        name: string,
+        age: number,
+        gender: string,
+        email: string,
+        phone?: string,
+    },
+    token : string,
+}
+
+export class RegisterUserUseCase {
+    private createUserRepo: CreateUser;
+    private jsonWebTokenGenerator: GenerateJSONWebToken;
+
+    constructor(createUserRepo: CreateUser, jsonWebTokenGenerator: GenerateJSONWebToken) {
+        this.createUserRepo = createUserRepo;
+        this.jsonWebTokenGenerator = jsonWebTokenGenerator;
+    }
+
+    private toCreateUserUseCaseResponse(user: User, token: string): RegisterUserUseCaseResponse {
+        return {
+            user: {
+                name: user.name,
+                age: user.age,
+                gender: String(user.gender),
+                email: user.email,
+                phone: user.phone,
+            },
+            token,
+        }
+    }
+
+    public async createUser(userData: RegisterUserUseCaseRequest) {
+        // TODO:-> duplicate email checking
+        const user = await this.createUserRepo.createUser(
+            User.NewUser(userData),
+        );
+
+        const token = await this.jsonWebTokenGenerator.generateJSONWebTOken(user);
+
+        return this.toCreateUserUseCaseResponse(user, token);
+    }
+}
+
+export const newRegisterUserUseCase = (createUserRepo: CreateUser, jsonWebTokenGenerator: GenerateJSONWebToken) => {
+    return new RegisterUserUseCase(createUserRepo, jsonWebTokenGenerator);
+}
