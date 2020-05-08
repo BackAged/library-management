0a6ede82eda4ed5e9af130d6fce5892bb5ae3e2d
BackAged <brotherapes@gmail.com>
2020-05-08 22:21:27 +0600

    Implement web handler utis

--- book/application/rest/util.go
+++ book/application/rest/util.go
@@ -3,12 +3,13 @@ package rest
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 // Response reponse serializer util
 type Response struct {
 	Status  int         `json:"-"`
-	Message string      `json:"title,omitempty"`
+	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
 	Errors  interface{} `json:"errors,omitempty"`
 }
@@ -30,4 +31,63 @@ func ServeJSON(status int, message string, data interface{}, errors interface{},
 	}
 
 	return nil
+}
+
+// ParseSkipLimit parses query from query string
+func ParseSkipLimit(r *http.Request) (map[string]int64, error) {
+	m := make(map[string]int64)
+	ve := &ValidationError{}
+
+	s := r.URL.Query().Get("skip")
+	if s != "" {
+		skip, err := strconv.ParseInt(s, 10, 64)
+		sErr := []string{}
+		if err != nil {
+			sErr = append(sErr, "skip must be a integer")
+		} else {
+			if skip < 0 {
+				sErr = append(sErr, "skip must be a positive integer")
+			}
+		}
+
+		if len(sErr) == 0 {
+			m["skip"] = skip
+		} else {
+			ve.Add("skip", sErr)
+		}
+	} else {
+		m["skip"] = 0
+	}
+
+	l := r.URL.Query().Get("limit")
+	if l != "" {
+		l := r.URL.Query().Get("limit")
+		limit, err := strconv.ParseInt(l, 10, 64)
+		lErr := []string{}
+		if err != nil {
+			lErr = append(lErr, "limit must be a integer")
+		} else {
+			if limit < 0 {
+				lErr = append(lErr, "limit must be a positive integer")
+			}
+
+			if limit > 50 {
+				limit = 25
+			}
+		}
+
+		if len(lErr) == 0 {
+			m["limit"] = limit
+		} else {
+			ve.Add("limit", lErr)
+		}
+	} else {
+		m["limit"] = 50
+	}
+
+	if len(*ve) == 0 {
+		return m, nil
+	}
+
+	return nil, ve
 }
