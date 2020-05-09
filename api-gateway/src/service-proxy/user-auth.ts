b1d778f5409dcab0e15dbb87847bfbddc5ce82d7
BackAged <brotherapes@gmail.com>
2020-05-09 14:38:27 +0600

    Implement service wise utils

---
+++ api-gateway/src/service-proxy/user-auth.ts
@@ -0,0 +1,11 @@
+import configuration from "../configuration";
+
+const BaseURL = configuration.USER_AUTH_URL;
+
+export const USER_AUTH_URLS = {
+    login : `${BaseURL}/api/v1/auth/login`,
+    register: `${BaseURL}/api/v1/auth/register`,
+    verifyToken: `${BaseURL}/api/v1/auth/verify-token`,
+    listUser: `${BaseURL}/api/v1/user`,
+    getUser: (param: string) => `${BaseURL}/api/v1/user/${param}`,
+}
