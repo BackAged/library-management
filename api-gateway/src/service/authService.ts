4bc8abb0737b3520783d0e9ea847b4dede22e26e
BackAged <brotherapes@gmail.com>
2020-05-09 14:38:40 +0600

    Implement authorization service

---
+++ api-gateway/src/service/authService.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { USER_AUTH_URLS } from "../service-proxy/user-auth";
+
+
+
+export const getUserFromToken = async (token: string) => {
+    try {
+        const response = await axios.get(USER_AUTH_URLS.verifyToken, {headers:{
+            "authorization": token,
+        }})
+        return response.data;
+    } catch (e) {
+        throw e;
+    }   
+}
